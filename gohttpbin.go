cd59eff8fa0964941bf6bdabbda10305282222d5
Cody Soyland <cody@soyland.org>
2013-03-24 22:31:09 -0500

    Add /stream and /delay handlers.

--- gohttpbin.go
+++ gohttpbin.go
@@ -7,6 +7,9 @@ import (
     "strings"
     "encoding/json"
     "compress/gzip"
+    "regexp"
+    "strconv"
+    "time"
 )
 
 type Headers map[string]string
@@ -73,6 +76,50 @@ func gzipHandler(w http.ResponseWriter, r *http.Request) {
     defer writer.Close()
 }
 
+func streamHandler(w http.ResponseWriter, r *http.Request) {
+    re := regexp.MustCompile(`/stream/(\d{1,9}).*`)
+    match := re.FindStringSubmatch(r.URL.String())
+    if len(match) == 0 {
+        w.WriteHeader(404)
+        return
+    }
+    res := buildResponseDict(r, []string{"url", "args", "headers", "origin"})
+
+    w.Header().Set("Transfer-Encoding", "chunked")
+    w.Header().Set("Content-Type", "application/json")
+
+    count, err := strconv.Atoi(match[1])
+    if err != nil { log.Fatal(err) }
+    for i := 0; i < count; i++ {
+        res["id"] = i
+        response, err := json.Marshal(res)
+        if err != nil { log.Fatal(err) }
+        response = append(response, byte(10))
+
+        w.Write(response)
+        w.(http.Flusher).Flush()
+    }
+}
+
+func delayHandler(w http.ResponseWriter, r *http.Request) {
+    re := regexp.MustCompile(`/delay/(\d{1,2}).*`)
+    match := re.FindStringSubmatch(r.URL.String())
+    if len(match) == 0 {
+        w.WriteHeader(404)
+        return
+    }
+    length, err := strconv.Atoi(match[1])
+    if err != nil { log.Fatal(err) }
+    if length > 10 {
+        length = 10
+    }
+    time.Sleep(time.Second*time.Duration(length))
+    res := buildResponseDict(r, []string{"url", "args", "form", "data", "origin", "headers", "files"})
+    response, err := json.MarshalIndent(res, "", "  ")
+    if err != nil { log.Fatal(err) }
+    fmt.Fprintf(w, string(response))
+}
+
 func buildResponseDict(r *http.Request, items []string) ResponseDict {
     res := make(ResponseDict)
     for _, item := range items {
@@ -83,6 +130,9 @@ func buildResponseDict(r *http.Request, items []string) ResponseDict {
             case "origin": res[item] = getIp(r)
             case "gzipped": res[item] = true
             case "method": res[item] = r.Method
+            case "form": res[item] = make(map[string]string) // TODO: implement
+            case "data": res[item] = "" // TODO: implement
+            case "files": res[item] = make(map[string]string) // TODO: implement
         }
     }
     return res
@@ -95,6 +145,8 @@ func main() {
     http.HandleFunc("/headers", headers)
     http.HandleFunc("/get", get)
     http.HandleFunc("/gzip", gzipHandler)
+    http.HandleFunc("/stream/", streamHandler)
+    http.HandleFunc("/delay/", delayHandler)
 
     fmt.Printf("Listening on port 8000...\n")
     err := http.ListenAndServe(":8000", nil)
